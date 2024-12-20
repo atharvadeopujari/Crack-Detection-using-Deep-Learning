8fe4a996027c77ad76d64813cba4d79f1a3975ce
atharvadeopujari <atharvadeopujari@gmail.com>
2024-12-20 10:02:32 +0530

    added for loop in single_comparision.py

--- single_comparision.py
+++ single_comparision.py
@@ -1,88 +1,49 @@
 import numpy as np
-import math
 from PIL import Image
 import tensorflow as tf
 from tensorflow import keras
-from keras import layers
-from keras.models import Model
 import matplotlib.pyplot as plt
-from random import randint
+from random import sample
 from dataset import img_read_fun, mask_read_fun
 
-
+# Directories
 img_dir = "C:\\Users\\user\\Desktop\\Atharva Deopujari\\CIT_data_1500\\DCW_1500\\"
 img_dir_mask = "C:\\Users\\user\\Desktop\\Atharva Deopujari\\CIT_data_1500\\mask_1500\\"
+# img_dir = "C:\\Users\\user\\Desktop\\Atharva Deopujari\\CIT RB\\png\\img\\"
+# img_dir_mask = "C:\\Users\\user\\Desktop\\Atharva Deopujari\\CIT RB\\png\\mask\\"
 
+# Load the model
 net = keras.models.load_model('trained_model.keras', compile=False)
 net.summary()
 
-i = 1200
-img_name = img_dir + str(i) + '.png'
-img_array = img_read_fun(img_name) 
-x = np.zeros((1, img_array.shape[0], img_array.shape[1], 1))
-x[0] = img_array
+total_images = 1500
+random_indices = sample(range(total_images), 10)
 
-mask_name = img_dir_mask + str(i) + '.png'
-img_mask_array = mask_read_fun(mask_name)
-y = np.zeros_like(x)
-y[0] = img_mask_array
+for i in random_indices:
+    img_name = img_dir + str(i) + '.png'
+    img_array = img_read_fun(img_name) 
+    x = np.zeros((1, img_array.shape[0], img_array.shape[1], 1))
+    x[0] = img_array
 
-z = net.predict(x)
-z[0, :, :, 0] = (z[0, :, :, 0] > 0.5).astype(np.float32)  # Apply threshold
+    mask_name = img_dir_mask + str(i) + '.png'
+    img_mask_array = mask_read_fun(mask_name)
+    y = np.zeros_like(x)
+    y[0] = img_mask_array
 
-plt.figure(1)
-plt.imshow(x[0, :, :, 0], cmap='gray')
-plt.title("Input Image")
+    z = net.predict(x)
+    z[0, :, :, 0] = (z[0, :, :, 0] > 0.5).astype(np.float32)  # Apply threshold
 
-plt.figure(2)
-plt.imshow(y[0, :, :, 0], cmap='gray')
-plt.title("Ground Truth Mask")
+    plt.figure(figsize=(15, 5))
+    plt.subplot(1, 3, 1)
+    plt.imshow(x[0, :, :, 0], cmap='gray')
+    plt.title(f"Input Image {i}")
 
-plt.figure(3)
-plt.imshow(z[0, :, :, 0], cmap='gray')
-plt.title("Predicted Mask")
+    plt.subplot(1, 3, 2)
+    plt.imshow(y[0, :, :, 0], cmap='gray')
+    plt.title(f"Ground Truth Mask {i}")
 
-plt.show()
+    plt.subplot(1, 3, 3)
+    plt.imshow(z[0, :, :, 0], cmap='gray')
+    plt.title(f"Predicted Mask {i}")
 
-
-# no = 1
-
-# h = 2048
-# w = 2560
-
-# x=np.zeros((no,h,w,1))
-# y=np.zeros((no,h,w,1))
-# z=np.zeros((no,h,w,1))
-
-# net=keras.models.load_model('trained_model.keras', compile=False)
-
-# net.summary(150)
-
-# i=1200
-
-# img_name = img_dir + str(i) + '.png'
-# img = Image.open(img_name)
-# img_array = np.array(img)
-
-# mask_name = img_dir_mask + str(i) + '.png'
-# img_mask = Image.open(mask_name)
-# img_mask_array = np.array(img_mask)
-# g = img_array
-# y[0, :, :, 0] = img_mask_array
-# x[0, :, :, 0] = g/255
-# z = net.predict(x)
-
-# img = Image.fromarray(x[0, :, :, 0]*255)
-# plt.figure(1)
-# plt.imshow(img)
-
-# img = Image.fromarray(z[0, :, :, 0]*255)
-# plt.figure(2)
-# plt.imshow(img)
-
-# img = Image.fromarray(y[0, :, :, 0]*255)
-# plt.figure(3)
-# plt.imshow(img)
-
-# plt.show()
-
+    plt.show()
